f69af1be048ea00158039c82ff2c1229f56a4d3a
barathm <barathm@opentext.com>
2024-12-04 14:26:49 +0530

    Initial commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/test';
-import BVS_Config from './src/com/testing//bvs/bvs.config';
-import AI_Config from './src/com/testing/ai/ai.config';
 
 export default defineConfig({
 
   projects:[
 
     {
-      name: 'ai',
+      name: 'Project1',
 
     },
 
     {
-      name: 'bvs',
-      ...BVS_Config,
+      name: 'Project2',
+    
 
     }
   ]
